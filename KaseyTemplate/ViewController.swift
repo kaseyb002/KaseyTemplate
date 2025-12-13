b8007e561d6a78d1ebf380a5723d7d751336287e
Kasey Baughan <kaseyb002@yahoo.com>
2025-12-13 16:17:31 -0500

    Initial Commit

---
+++ KaseyTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KaseyTemplate
+//
+//  Created by Kasey Baughan on 12/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KaseyTemplateTests/KaseyTemplateTests.swift
@@ -0,0 +1,17 @@
+//
+//  KaseyTemplateTests.swift
+//  KaseyTemplateTests
+//
+//  Created by Kasey Baughan on 12/13/25.
+//
+
+import Testing
+@testable import KaseyTemplate
+
+struct KaseyTemplateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
